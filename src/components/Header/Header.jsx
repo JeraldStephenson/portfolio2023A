f5815206b290fe3e1e1644acc518dfe62ad79742
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-02-25 14:03:41 -0800

    set up links to sections to use in header component, changed html title to reflect the probably domain name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import css from './Header.module.scss';
+import { BiPhoneCall } from 'react-icons/bi';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={`paddings ${css.wrapper}`}>
+      <div className={`flexCenter innerWidth ${css.container}`}>
+        <div className={css.name}>Jerald</div>
+        <ul>
+          <li>
+            <a href=''>About Me</a>
+            {/* download resume - quick snippet about myself */}
+          </li>
+          <li>
+            <a href=''>Experience</a>
+          </li>
+          <li>
+            <a href=''>Portfolio</a>
+          </li>
+          <li>
+            <p>+1 (209) 923-7926</p>
+            <BiPhoneCall size={'40px'} />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
