f5b129850ede3e86370c8bf137d4a98fadbe0aba
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-02-25 21:50:19 -0800

    added placeholders for cv/resume download, placeholder profile image for now. Added email link that opens email to my address

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,15 +5,26 @@ const Hero = () => {
   return (
     <section className={`paddings ${css.wrapper}`}>
       <div className={`innerWidth ${css.container}`}>
+        {/* upper elements */}
         <div className={css.upperElements}>
-          <span>
+          <span className='primaryText'>
             Hey There, <br /> I'm Jerald.
           </span>
-          <span>
+          <span className='secondaryText'>
             I am a full-stack engineer <br />
             and I love what I do
           </span>
         </div>
+        {/* person image */}
+        <div className={css.person}>
+          <img src='./profilepic.png' alt='profile pic of Jerald Stephenson' />
+        </div>
+        {/* email */}
+        <a href='mailto:jstephenson.dev@gmail.com' className={css.email}>
+          jstephenson.dev@gmail.com
+        </a>
+
+        {/* lower elements */}
         <div className={css.lowerElements}>
           <div className={css.experience}>
             <div className='primaryText'>01</div>
