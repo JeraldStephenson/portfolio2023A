cebe3b68ce7893a8cab49283c214b864217ed66d
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-04-08 21:45:51 -0700

    stylized form on contact section. made heading on contact section consistent with other sections.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import css from './Contact.module.scss';
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn, slideIn } from '../../utils/motion';
 
 const Contact = () => {
   return (
     <section id='contact' className={`paddings ${css.wrapper}`}>
       <a className='anchor' id='contact'></a>
       <div className={`${css.container} innerWidth `}>
-        <h5>Get In Touch</h5>
-        <h2>Contact Me</h2>
-        <form action='' className='innerWidth'>
+        {/* <h5>Get In Touch</h5>
+        <h2>Contact Me</h2> */}
+        <div className={`flexCenter ${css.heading}`}>
+          <div>
+            <span className='primaryText'>Contact Me</span>
+          </div>
+          {/* delete a tag if dont plan on linking defualt email app */}
+          <a
+            href='mailto:jstephenson.dev@gmail.com'
+            target={'_blank'}
+            className='secondaryText'
+          >
+            {/* Email */}
+          </a>
+        </div>
+        <div className={` ${css.projectType}`}>
+          <motion.span variants={fadeIn('right', 'tween', 0.5, 0.6)}>
+            Send me an Email
+          </motion.span>
+        </div>
+        <form action='' className='yPaddings'>
           <input
             type='text'
             name='name'
