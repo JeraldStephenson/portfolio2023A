6a78667f70d910a53e7cbcb1681d0ae68b73371d
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-04-18 21:01:13 -0700

    changed focus on inputs for contact sheet. Integrated EmailJS API to email form.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import css from './Contact.module.scss';
 import { motion } from 'framer-motion';
 import { staggerContainer, fadeIn, slideIn } from '../../utils/motion';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <section id='contact' className={`paddings ${css.wrapper}`}>
       <a className='anchor' id='contact'></a>
-      <div className={`${css.container} innerWidth `}>
+      <div className={`${css.container} innerWidth`}>
         {/* <h5>Get In Touch</h5>
         <h2>Contact Me</h2> */}
-        <div className={`flexCenter ${css.heading}`}>
+        <div className={`flexCenter  ${css.heading}`}>
           <div>
             <span className='primaryText'>Contact Me</span>
           </div>
@@ -28,7 +51,7 @@ const Contact = () => {
             Send me an Email
           </motion.span>
         </div>
-        <form action='' className='yPaddings'>
+        <form ref='form' className='yPaddings flexCenter' onSubmit={sendEmail}>
           <input
             type='text'
             name='name'
